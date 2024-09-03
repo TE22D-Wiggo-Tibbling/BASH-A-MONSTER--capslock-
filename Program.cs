5c236bb7470cf43fbcc41e3d336ddad8bd595674
Wiggo Tibbling <wiggo.tibbling@elev.ga.ntig.se>
2024-09-03 10:15:20 +0200

    HEJ HEJ MICKE (capslock)

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Skill attack = new() 
+{
+Name = "Attack enemy",
+ManaCost = 10
+};
+
+Skill heal = new() 
+{
+    Name = "heal self",
+    ManaCost = 20
+};
+
+Skill active = attack;
+
+System.Console.WriteLine(active.Name);
+Console.ReadLine();
---
+++ skill.cs
@@ -0,0 +1,5 @@
+public class Skill
+{
+    public string Name;
+    public int ManaCost;
+}
